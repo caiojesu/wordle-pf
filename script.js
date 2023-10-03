c57a128d3e0512f3934305c9a3424605558c2477
caiojesu <117685339+caiojesu@users.noreply.github.com>
2023-10-03 23:00:06 +0000

    Refatora funcao initBoard

--- script.js
+++ script.js
@@ -10,19 +10,24 @@ console.log(rightGuessString);
 
 function initBoard() {
   let board = document.getElementById("game-board");
+ createBoard(board);
+}
 
+function createBoard(board){
   for (let i = 0; i < NUMBER_OF_GUESSES; i++) {
     let row = document.createElement("div");
     row.className = "letter-row";
-
     for (let j = 0; j < 5; j++) {
-      let box = document.createElement("div");
-      box.className = "letter-box";
-      row.appendChild(box);
+    createColumns(row);
     }
-
     board.appendChild(row);
   }
+}
+
+function createColumns(row){
+    let box = document.createElement("div");
+    box.className = "letter-box";
+    row.appendChild(box);
 }
 
 function shadeKeyBoard(letter, color) {
