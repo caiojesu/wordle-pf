c95d9822bb9dec7f7209b27155295b74d20dae80
andrefelipedesantana <146356350+andrefelipedesantana@users.noreply.github.com>
2023-10-04 05:40:24 +0000

    substituição da variável let pela variável const, afim de respeitar o princípio da imutabilidade, e declaração de uma função do tipo arrow com o uso de const.

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ const NUMBER_OF_GUESSES = 6;
 let guessesRemaining = NUMBER_OF_GUESSES;
 let currentGuess = [];
 let nextLetter = 0;
-let rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
+const rightGuessString = WORDS[Math.floor(Math.random() * WORDS.length)];
 
 console.log(rightGuessString);
 
-function initBoard() {
-  let board = document.getElementById("game-board");
+const initBoard = () => {
+  const board = document.getElementById("game-board");
  createBoard(board);
 }
 
