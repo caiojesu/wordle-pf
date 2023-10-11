65d3808806ddde2ac5f9f712d002c21b40cc1307
caiojesu <117685339+caiojesu@users.noreply.github.com>
2023-10-11 09:37:27 +0000

    Refatora a função getBox utilizando Currying

--- modules/dom_controller.js
+++ modules/dom_controller.js
@@ -1,12 +1,11 @@
 // Retorna a linha atual com base no número de tentativas restantes.
 export const getRow = (guessesRemaining) => {
-    console.log(guessesRemaining);
     return document.getElementsByClassName("letter-row")[6 - guessesRemaining];
 };
   
 // Retorna a caixa atual de acordo com a linha escolhida e a próxima caixa.
-export const getBox = (row, nextLetter) => {
-    return row.children[nextLetter - 1];
+export const getBox = (row) => (index) => {
+    return row.children[index];
 };
 
 // Limpa o conteúdo da caixa escolhida 
