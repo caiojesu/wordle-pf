32398b6c994cf9b276965a9bc538f0ff6e9a1f19
andrefelipedesantana <146356350+andrefelipedesantana@users.noreply.github.com>
2023-10-12 21:27:28 +0000

    substituição de functions por const e adição de função .push para adicionar  todos os elementos de um array à lista WORDS

--- modules/words.js
+++ modules/words.js
@@ -2,32 +2,32 @@ const ARRAY_ANIMAIS = ['panda', 'porco', 'pulga'];
 const ARRAY_OBJETOS = ['bolsa', 'pente', 'mouse'];
 const ARRAY_NOMES = ['pedro', 'andre', 'lucas'];
 const ARRAY_VERBOS = ['matar', 'comer', 'beber'];
-export let WORDS = ['garfo', 'foice', 'jarro' ];
+export const WORDS = ['garfo', 'foice', 'jarro'];
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const botao1 = document.getElementById("botao-1");
   const botao2 = document.getElementById("botao-2");
   const botao3 = document.getElementById("botao-3");
   const botao4 = document.getElementById("botao-4");
 
-  function atualizarWords(array) {
-    WORDS = [...array];
+  const atualizarWords = (array) => {
+    WORDS.push(...array);
     console.log("WORDS atualizado com sucesso.");
   }
 
-  botao1.addEventListener("click", function () {
+  botao1.addEventListener("click", () => {
     atualizarWords(ARRAY_ANIMAIS);
   });
 
-  botao2.addEventListener("click", function () {
+  botao2.addEventListener("click", () => {
     atualizarWords(ARRAY_OBJETOS);
   });
 
-  botao3.addEventListener("click", function () {
+  botao3.addEventListener("click", () => {
     atualizarWords(ARRAY_NOMES);
   });
 
-  botao4.addEventListener("click", function () {
+  botao4.addEventListener("click", () => {
     atualizarWords(ARRAY_VERBOS);
   });
 });
